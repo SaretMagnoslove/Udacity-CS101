8d2ec71231c61332cb4d6554f9ebf03c9687acfd
SaretMagnoslove <magnoslove@gmail.com>
2018-03-29 20:54:42 +0300

    Unit03  optional problem set code

---
+++ Unit03/trainFocus.py
@@ -0,0 +1,10 @@
+# code for train focus quiz
+s = "CidatyUcityda"
+
+print (s[6]+s[-2]+s[7:12] == 'Udacity')
+print (s[6]+s[-2:]+s[7:11] == 'Udacity')
+print (s[6]+s[2:4]+s[7:13] == 'Udacity')
+
+print (s[-7]+s[2:4]+s[7:11] == 'Udacity')
+print (s[6]+s[-2]+s[3]+s[:2]+s[4:6]  == 'Udacity')
+print (s[6]+s[2]+s[3]+s[7:11] == 'Udacity')
