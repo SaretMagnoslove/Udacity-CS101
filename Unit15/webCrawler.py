13ec93897b4c50999d03b93b5bc6cf8be06463d3
SaretMagnoslove <magnoslove@gmail.com>
2018-06-02 16:34:00 +0300

    ADD Unit15Code

---
+++ Unit15/webCrawler.py
@@ -0,0 +1,17 @@
+from addPapeToIndex import add_page_to_index
+from crawlWeb import get_page
+from crawlWeb import union
+from crawlWeb import get_all_links
+
+def crawl_web(seed):
+    tocrawl = [seed]
+    crawled = []
+    index = []
+    while tocrawl:
+        page = tocrawl.pop()
+        if page not in crawled:
+            content = get_page(page)
+            add_page_to_index(index, page, content)
+            union(tocrawl, get_all_links(content))
+            crawled.append(page)
+    return index
